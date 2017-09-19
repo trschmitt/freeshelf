760f5874f48a92f8233a14bd0dba0e54fe82768d
Tristan Schmitt <trschmitt50@live.com>
2017-09-19 11:21:44 -0400

    Added User Model with secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905192358) do
+ActiveRecord::Schema.define(version: 20170918202335) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "description"
     t.string "URL"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
