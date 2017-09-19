7c1ed95423bc99941d5dcce9dd48e160094e3272
Tristan Schmitt <trschmitt50@live.com>
2017-09-19 13:08:56 -0400

    Finished Sessions controller. User can now log in and view books

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_uniqueness_of :email
+
+  before_validation :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase if email
+  end
 end
