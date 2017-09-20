7580fe809d935c6a4bd687111fa18ef2a501f355
Tristan Schmitt <trschmitt50@live.com>
2017-09-20 11:50:01 -0400

    Fixed login and started associations with books and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918202335) do
+ActiveRecord::Schema.define(version: 20170920150323) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170918202335) do
     t.string "URL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
